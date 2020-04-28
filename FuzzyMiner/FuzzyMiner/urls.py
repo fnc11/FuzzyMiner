e7b9a4fbae022a9c03b7c25a22fc21ad076de95c
Eric Li <eric.pireirik@gmail.com>
2020-04-28 18:21:47 +0200

    Add miner for views

--- FuzzyMiner/FuzzyMiner/urls.py
+++ FuzzyMiner/FuzzyMiner/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from . import view
+from django.views.generic import TemplateView
+from miner import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', view.home),
-    url(r'^filter$', view.filter),
-    url(r'^help$', view.help)
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
---
+++ FuzzyMiner/secret.py
@@ -0,0 +1,7 @@
+#!python3
+# -*- coding=UTF-8 -*-
+
+# Create by Eric Li at 28.04.20 for FuzzyMiner
+from django.core.management import utils
+
+print(utils.get_random_secret_key())
