324759ec76a8335a3c7b8244fa9e9516c8c4dfd6
Praveen Yadav <inventthenext@gmail.com>
2020-05-04 11:04:06 +0200

    Added some view changes

--- FuzzyMiner/miner/views.py
+++ FuzzyMiner/miner/views.py
@@ -2,6 +2,8 @@ import json
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from fuzzyminerpk import FuzzyMiner
+from django.shortcuts import render
+from pm4py.objects.log.importer.xes import factory as xes_import_factory
 
 # Create your views here.
 @csrf_exempt
@@ -14,5 +16,17 @@ def upload(request):
     return HttpResponse()
 
 
-def launch_filter(logs):
-    pass
+def launch_filter(logs_file):
+    # log = xes_import_factory.apply('<path_to_xes_file>')
+    log = xes_import_factory.apply(logs_file)
+    return log[0]
+
+
+def handle_file(request):
+    return render(request, 'inp_miner.html')
+
+
+def show_result(request):
+    logs_file = request.FILES['logs_file']
+    resp = launch_filter(logs_file)
+    return render(request, 'res_miner.html', {'result': resp})
