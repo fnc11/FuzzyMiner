34992b24f1b76b843a47b473f345976dc88c400f
Pruthvi <pruthvihegde93@gmail.com>
2020-05-06 00:11:56 +0200

    Adding banner and footer background and some other styling changes

--- miner-web/src/App.vue
+++ miner-web/src/App.vue
@@ -1,29 +1,33 @@
-<template>
+<template xmlns:el-col="http://www.w3.org/1999/html">
     <div id="app">
         <el-container>
-            <el-header>
-                <el-row class="row-bg">
+            <el-header class="header-bg">
+                <el-row>
                     <el-col :span="20">
-                        <div class="grid-content bg-purple-dark">
+                        <div class="grid-content" align="left">
                             <label>FuzzyMinerWeb</label>
                         </div>
                     </el-col>
+
                     <el-col :span="4">
-                        <el-button-group>
-                            <el-button :disabled="$route.name === 'Home'" @click="$router.push({path: '/'})">Home
-                            </el-button>
-                            <el-button :disabled="$route.name === 'Help'" @click="$router.push({path: '/help'})">
-                                Help
-                            </el-button>
-                        </el-button-group>
+                        <div align="right" class="button-group">
+                            <el-button-group>
+                                <el-button :disabled="$route.name === 'Home'" @click="$router.push({path: '/'})">Home
+                                </el-button>
+                                <el-button :disabled="$route.name === 'Help'" @click="$router.push({path: '/help'})">
+                                    Help
+                                </el-button>
+                            </el-button-group>
+                        </div>
                     </el-col>
+
                 </el-row>
             </el-header>
 
             <el-main>
                 <router-view/>
             </el-main>
-            <el-footer class="fluid-container footer">
+            <el-footer class="fluid-container footer" height="5%">
                 <p>@Copy right: 2020</p>
             </el-footer>
         </el-container>
@@ -39,21 +43,49 @@
 </script>
 
 <style scoped>
-    .grid-content{
+    .header-bg {
+        top: 0;
+        position: absolute;
+        width: 100%;
+        height: 60px;
+        background-color: teal;
+    }
+
+    .grid-content {
         font-family: "Sitka Heading";
-        font-size: x-large;
-        color: midnightblue;
+        font-size: xx-large;
+        color: black;
         font-weight: bold;
+        line-height: 1.7;
 
     }
+
     .fluid-container.footer {
         bottom: 0;
         position: absolute;
         width: 100%;
-        height: 60px;
+        background-color: teal;
+        height: 20%;
+
     }
 
     .fluid-container.footer > *:last-child {
         text-align: center;
+
+
     }
+
+    .button-group {
+        position: absolute;
+        bottom: 0;
+
+    }
+
+    .el-button {
+
+        border-radius: 0.2px;
+
+
+    }
+
 </style>
--- miner-web/src/components/Home.vue
+++ miner-web/src/components/Home.vue
@@ -4,7 +4,7 @@
             <el-row class="banner-content">
                 <h1>One step to Mine the Fuzzy Model!</h1>
             </el-row>
-            <el-row type="flex" justify="center">
+            <el-row class="button-class" justify="center" type="flex">
                 <el-col :span="2.5">
                     <el-upload
                             ref="upload"
@@ -13,23 +13,23 @@
                             accept=".xes"
                             :http-request="upload"
                             :auto-upload="true">
-                        <el-button type="primary">Upload Logs</el-button>
+                        <el-button type="primary" class="button-primary">Upload Logs</el-button>
                         <div slot="tip" class="el-upload__tip">Accepted file format is .xes.</div>
                     </el-upload>
                 </el-col>
                 <el-col :span="2.5">
-                    <el-button type="success" @click="generate" disabled>Generate</el-button>
+                    <el-button type="success" class="button-success" @click="generate" disabled>Generate</el-button>
                 </el-col>
             </el-row>
         </div>
         <!--
-        <div>
-            <h3>Process Mining</h3>
-        </div>
-        <div>
-            <h3>Fuzzy Miner</h3>
-        </div>
-        -->
+    <div>
+        <h3>Process Mining</h3>
+    </div>
+    <div>
+        <h3>Fuzzy Miner</h3>
+    </div>
+    -->
     </div>
 </template>
 
@@ -54,12 +54,25 @@
 
 <style scoped>
     .main {
-        height: 83vh;
+        height: 60vh;
         display: flex;
         justify-content: center;
         align-items: center;
+        
     }
-    .banner-content{
+
+    .banner-content {
         text-align: center;
+    }
+
+    .button-class {
+        height: inherit;
+        top: 5vh;
+
+    }
+
+    .button-primary {
+        background-color: cornflowerblue;
+
     }
 </style>
