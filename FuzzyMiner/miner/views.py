e3c6714bada353741e67a0fff02e3df1767ae647
PrantikC <prantikchatterjee22@gmail.com>
2020-05-05 18:38:45 +0200

    Added absolute path for the file import

--- FuzzyMiner/miner/views.py
+++ FuzzyMiner/miner/views.py
@@ -3,21 +3,25 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from pm4py.objects.log.importer.xes import factory as xes_import_factory
+from path import Path
 
 # Create your views here.
+# Hard coding the log file path
+logs = Path('D:\RWTH\PM Lab\BPI Assignment 2.xes').abspath()
 @csrf_exempt
 def upload(request):
     # get file from form data
-    file = request.FILES.get('file')
+
+    file = request.FILES.get(logs)
     print(file.name)
     # read data, it is binary data
     print(file.read())
     return HttpResponse()
 
 
-def launch_filter(logs_file):
-    # log = xes_import_factory.apply('<path_to_xes_file>')
-    log = xes_import_factory.apply(logs_file)
+def launch_filter(logs):
+    #log = xes_import_factory.apply('D:\RWTH\PM Lab\BPI Assignment 2.xes.gz')
+    log = xes_import_factory.apply(logs)
     return log[0]
 
 
@@ -26,6 +30,6 @@ def handle_file(request):
 
 
 def show_result(request):
-    logs_file = request.FILES['logs_file']
-    resp = launch_filter(logs_file)
+    logs = request.FILES['logs']
+    resp = launch_filter(logs)
     return render(request, 'res_miner.html', {'result': resp})
