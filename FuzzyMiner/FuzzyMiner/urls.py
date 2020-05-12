1a48a004b92e966d6df527f13592be6b1f7aa103
Iftekhar Ahmed <bit0220iftekhar@gmail.com>
2020-05-12 14:52:51 +0200

    Fix miner package import in urls.py

--- FuzzyMiner/FuzzyMiner/urls.py
+++ FuzzyMiner/FuzzyMiner/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from miner import views
+from FuzzyMiner.miner import views
 
 
 urlpatterns = [
