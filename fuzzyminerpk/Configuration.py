6888a6d839be140b6e969270c4acecc793278fd5
PrantikC <prantikchatterjee22@gmail.com>
2020-05-13 10:54:35 +0200

    Unary and binary significance metric functions added

--- fuzzyminerpk/Configuration.py
+++ fuzzyminerpk/Configuration.py
@@ -4,53 +4,6 @@ class Configuration:
         self.filter_config = filter_config
         self.metric_configs = metric_configs
 
-    def weighted_configuration(self, fuzzy_config):
-        # aggregating configuration weights for metrics
-        node_sig_weights_freq = 0.5
-        node_sig_weights_routing = 0.5
-        sig_weights_freq = 0.5
-        sig_weights_dist = 0.5
-        corr_weights_prox = 0.5
-        corr_weights_endpoint = 0.5
-        corr_weights_originator = 0.5
-        corr_weights_datatype = 0.5
-        corr_weights_datavalue = 0.5
-        edge_sig_to_corr_ratio = 0.75
-
-        # calculating aggregate node significance values
-        agg_node_sig = (fuzzy_config.metric_configs[0] * node_sig_weights_freq) + (
-                    fuzzy_config.metric_configs[1] * node_sig_weights_routing)
-        max_agg_node_sig_value = max(agg_node_sig)
-        # normalized_values
-        agg_node_sig = agg_node_sig / max_agg_node_sig_value
-
-        # calculating aggregate edge significance values
-        agg_edge_sig = (fuzzy_config.metric_configs[2] * sig_weights_freq) + (
-                    fuzzy_config.metric_configs[3] * sig_weights_dist)
-        max_agg_edge_sig_value = max(agg_edge_sig)
-        # normalized values
-        agg_edge_sig = agg_edge_sig / max_agg_edge_sig_value
-
-        # calculating aggregate edge correlation values
-        agg_edge_corr = (fuzzy_config.metric_configs[4] * corr_weights_prox) + \
-                        (fuzzy_config.metric_configs[5] * corr_weights_endpoint) + \
-                        (fuzzy_config.metric_configs[6] * corr_weights_datatype) + \
-                        (fuzzy_config.metric_configs[7] * corr_weights_datavalue) + \
-                        (fuzzy_config.metric_configs[8] * corr_weights_originator)
-        max_agg_edge_corr_value = max(agg_edge_corr)
-        # normalized values
-        agg_edge_corr = agg_edge_corr / max_agg_edge_corr_value
-
-        # Taking weighted average of agg_edge_sig and agg_edge_corr
-        agg_edge_values = (agg_edge_sig * edge_sig_to_corr_ratio) + agg_edge_corr * (1 - edge_sig_to_corr_ratio)
-        max_agg_edge_value = max(agg_edge_values)
-        # normalize values
-        agg_edge_values = agg_edge_values / max_agg_edge_value
-        aggregate_weights = [agg_node_sig, agg_edge_sig, agg_edge_corr, agg_edge_values]
-
-        fuzzy_config_weighted = MetricWeight(aggregate_weights, True, False)
-        return fuzzy_config_weighted
-
 
 class FilterConfig:
     def __init__(self, node_filter, edge_filter, concurrency_filter):
@@ -60,11 +13,12 @@ class FilterConfig:
 
 
 class MetricConfig:
-    def __init__(self, name, metric_type, include=True, invert=False):
+    def __init__(self, name, metric_type, include=True, invert=False, weight=0.5):
         self.name = name
         self.metric_type = metric_type
         self.include = include
         self.invert = invert
+        self.weight = weight
 
 
 class MetricWeight:
--- fuzzyminerpk/FuzzyMiner.py
+++ fuzzyminerpk/FuzzyMiner.py
@@ -5,24 +5,24 @@ class Graph:
         self.nodes_dict = dict()
         self.nodes_index = dict()
         # Warning only 20 activities is considered at max
-        self.edges_list = [[0 for x in range(10)] for y in range(10)]
+        self.edges_list = [[0 for x in range(20)] for y in range(20)]
+        self.edge_distance_values = [[0 for x in range(20)] for y in range(20)]
         self.extract_dicts()
 
         # Metrics Operations
-        #uniary
+        # unary
         self.node_routing_values = dict()
         self.unary_sig_values = dict()
-        self.cal_uniary_metrics()
-        #binary
+        self.cal_unary_metrics()
+        # binary
         self.edge_distance_values = list()
         self.binary_sig_values = list()
         self.cal_binary_metrics()
-        #binary correlation
+        # binary correlation
         self.time_diff_values = list()
         self.resource_corr_values = list()
         self.activity_corr_values = list()
         self.cal_binary_corr_metrics()
-
 
         self.clusters = list()
         self.apply_config()
@@ -42,12 +42,59 @@ class Graph:
                 #     Add some kind of notifier to the user if activities exceed max num(20 for eg)
                 # calculating edges 2D matrix
                 if num > 1 and i > 0:
-                    self.edges_list[self.nodes_index[trace[i - 1]['concept:name']]][self.nodes_index[trace[i]['concept:name']]] += 1
+                    self.edges_list[self.nodes_index[trace[i - 1]['concept:name']]][
+                        self.nodes_index[trace[i]['concept:name']]] += 1
 
     def change_config(self, new_config):
         pass
 
     def apply_config(self):
+        pass
+
+    def cal_unary_metrics(self):
+        in_arcs = 0
+        out_arcs = 0
+        self.extract_dicts()
+        for row in range(len(self.edges_list)):
+            for col in range(len(self.edges_list[row])):
+                out_arcs += self.edges_list[row][col]
+                in_arcs += self.edges_list[col][row]
+                self.node_routing_values[row][col] = abs(in_arcs - out_arcs)
+
+        # Hard-coding the weights
+        node_sig_weights_freq = 0.5
+        node_sig_weights_routing = 0.5
+
+        # Finding the aggregate unary node significance
+        for key in self.nodes_dict:
+            if key in self.node_routing_values:
+                self.unary_sig_values[key] = (self.nodes_dict[key] * node_sig_weights_freq) + (self.node_routing_values[key] * node_sig_weights_routing)
+            else:
+                pass
+
+    def cal_binary_metrics(self):
+        self.extract_dicts()
+        for row in range(len(self.edges_list)):
+            for col in range(len(self.edges_list[row])):
+                a = self.edges_list[row][col]
+                n1 = self.nodes_dict[row]
+                n2 = self.nodes_dict[col]
+                self.edge_distance_values = abs((n1 + n2) - a)
+
+        # Hard-coding the weights
+        edge_sig_weights_freq = 0.5
+        edge_sig_weights_dist = 0.5
+
+        # Finding the aggregate binary edge significance
+        for key in self.nodes_dict:
+            if key in self.edge_distance_values:
+                self.unary_sig_values[key] = (self.nodes_dict[key] * edge_sig_weights_freq) + (self.edge_distance_values * edge_sig_weights_dist)
+            else:
+                pass
+
+        return self.unary_sig_values
+
+    def cal_binary_corr_metrics(self):
         pass
 
 
