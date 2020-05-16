3c3959ee6ba174904648846328e9192c2bb97c4f
Iftekhar Ahmed <bit0220iftekhar@gmail.com>
2020-05-16 21:38:33 +0200

    Finish file uploading

--- FuzzyMiner/miner/urls.py
+++ FuzzyMiner/miner/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('', views.handle_file, name="handle_file"),
-    path('upload', views.upload, name="upload")
+    path('upload', views.upload, name="upload"),
+    path('generate', views.show_result, name="generate")
 ]
