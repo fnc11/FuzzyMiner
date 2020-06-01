8e8492b3cffc5c3ea412a40510d0bd4876c7f83b
Praveen Yadav <heypraveenyadav@gmail.com>
2020-06-01 14:16:02 +0200

    Changed Fuzzy Edge Cut_off to preserve

--- fuzzyminerpk/Filter.py
+++ fuzzyminerpk/Filter.py
@@ -20,19 +20,19 @@ class NodeFilter(Filter):
 
 # Edge transform 0: best edges, 1: fuzzy edges
 class EdgeFilter(Filter):
-    def __init__(self, edge_transform=1, sc_ratio=0.75, cut_off=0.2,
+    def __init__(self, edge_transform=1, sc_ratio=0.75, preserve=0.2,
                  ignore_self_loops=True, interpret_abs=False):
         super().__init__("edge_filter")
         self.edge_transform = edge_transform
         self.sc_ratio = sc_ratio
-        self.cut_off = cut_off
+        self.preserve = preserve
         self.ignore_self_loops = ignore_self_loops
         self.interpret_abs = interpret_abs
 
     def __str__(self):
         if self.edge_transform == 1:
             return super().__str__() + " Edge Transform: " + str(self.edge_transform) + " sc_ratio: " + str(
-                self.sc_ratio) + " Cut Off: " + str(self.cut_off) + " Ignore Self Loops: " + str(
+                self.sc_ratio) + " Preserve: " + str(self.preserve) + " Ignore Self Loops: " + str(
                 self.ignore_self_loops) + " Interpret Absolute: " + str(self.interpret_abs)
         else:
             return super().__str__() + "Edge Transform: " + str(self.edge_transform)
