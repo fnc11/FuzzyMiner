e3a8ba36610e4fad6d7e8afb89b90eeee1ad5077
Pireirik <eric.pireirik@gmail.com>
2021-01-09 04:55:15 +0100

    Add configuration files for deployment

--- FuzzyMiner/FuzzyMiner/urls.py
+++ FuzzyMiner/FuzzyMiner/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('miner/', include('miner.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
